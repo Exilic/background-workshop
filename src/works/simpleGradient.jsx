ded4e868e596c913f5319c5acf90471f11b32e95
Exilic <tobias.engberg@exilic.se>
2023-05-18 23:18:08 +0200

    Simple gradient background

---
+++ src/works/simpleGradient.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './simpleGradient.css';
+
+const SimpleGradient = () => {
+    return (
+        <div className="container">
+        </div>
+    );
+};
+
+export default SimpleGradient;
