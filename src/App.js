9a1aa77dcf16a8b6fe46738b854370ebea14d7fe
Exilic <tobias.engberg@exilic.se>
2023-06-06 17:07:46 +0200

    Adaptive svg for optical pattern

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import FourCorners2 from "./works/fourCorners2";
+import OpArt from "./works/opArt";
 
 
 function App() {
   return (
     <div className="App">
-      <FourCorners2 />
+      <OpArt />
     </div>
   );
 }
---
+++ src/works/opArt.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './opArt.css';
+/*import left from '../img/optical-left.svg';
+import top from '../img/optical-top.svg';
+import right from '../img/optical-right.svg';
+import bottom from '../img/optical-bottom.svg';*/
+
+// https://stackoverflow.com/questions/12464101/svg-stretching-an-image
+// in svg, put => preserveAspectRatio="none"
+const OpArt = () => {
+    return (
+        <div id="optical-white">
+{/*            <img src={left} alt="optical pattern"/>
+            <img src={top} alt="optical pattern"/>
+            <img src={right} alt="optical pattern"/>
+            <img src={bottom} alt="optical pattern"/>*/}
+        </div>
+    );
+};
+
+export default OpArt;
