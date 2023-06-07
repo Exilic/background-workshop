bdfb17d5e37efcd704598979eeb1e9d18f0f1538
Exilic <tobias.engberg@exilic.se>
2023-06-08 00:39:16 +0200

    Vertical color bars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import FourCorners2 from "./works/fourCorners2";
 import OpArt from "./works/opArt";
+import ColorBars from "./works/colorBars";
 
 
 function App() {
   return (
     <div className="App">
-      <OpArt />
+      <ColorBars />
     </div>
   );
 }
---
+++ src/works/colorBars.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './colorBars.css';
+
+const ColorBars = () => {
+    return (
+        <div id="background-bars">
+        </div>
+    );
+};
+
+export default ColorBars;
